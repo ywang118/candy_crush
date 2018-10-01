c1cbf13efe6433791df7b7e0efcb78813f0dd013
Yixuan Wang <ywang0506@gmail.com>
2018-10-01 17:11:05 -0400

    First

---
+++ src/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var level = document.querySelectorAll('.grid-row')
+  let spriteNames = [
+    "Croissant",
+    "Cupcake",
+    "Danish",
+    "Donut",
+    "Macaroon",
+    "SugarCookie"]
+  const columns =document.querySelectorAll('.grid-row')[0].getElementsByClassName('grid-cell').length
+  const rows= document.querySelectorAll('.grid-row').length
+  function init(){
+    for(let i = 0; i < columns; i++){
+      
+    }
+  }
+}
